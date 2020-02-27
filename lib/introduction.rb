83c2a9e27d74acd5fa82050a8df3cd122b8c3023
Zeynab Jibreel <zeina2025@gmail.com>
2020-02-27 11:56:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
-def introduction (name = "Zeynab" , Language = "ruby")
+def introduction (name , Language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
